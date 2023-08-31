332e33526d76aa503069147b338dc74790120b0a
Avetis Avetisyan <avetisavetisyan116@gmail.com>
2023-08-31 15:36:05 +0400

    added ScrollList file

---
+++ lib/home/widgets/scroll_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScrollListHome extends StatefulWidget {
+  const ScrollListHome({super.key});
+
+  @override
+  State<ScrollListHome> createState() => _ScrollListHomeState();
+}
+
+class _ScrollListHomeState extends State<ScrollListHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('');
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home/widgets/appbar_home.dart';
+import 'home/widgets/scroll_list.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: [
           AppBarHome(),
+          ScrollListHome(),
         ],
       ),
     );
